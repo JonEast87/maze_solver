27da19319f40c21d79ac8047714c83357200c37c
JonEast87 <j.d.east@icloud.com>
2024-12-22 12:35:25 -0700

    missed some wrong coords in the cell from earlier, they have been corrected. opened the top and bottom of the maze up, will now work on breaking the inner walls to setup the maze.

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from cell import Cell
 from maze import Maze
 
 def main():
-	num_rows = 12
-	num_cols = 16
+	num_rows = 6
+	num_cols = 8
 	margin = 50
 	screen_x = 800
 	screen_y = 600
@@ -13,7 +13,6 @@ def main():
 	win = Window(screen_x, screen_y)
 
 	maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
-
 	win.wait_for_close()
 
 
